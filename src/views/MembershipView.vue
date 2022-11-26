2a782f1351291f8131100cf3907f9fac0f39e906
Azrialin <zxe2124@gmail.com>
2022-11-26 16:55:02 +0800

    tabs added

---
+++ src/views/MembershipView.vue
@@ -0,0 +1,20 @@
+<template lang="html">
+  <div>
+    <tabs>
+      <tab title="Tab 1">Hello From Tab 1</tab>
+      <tab title="Tab 2">Hello From Tab 2</tab>
+      <tab title="Tab 3">Hello From Tab 3</tab>
+    </tabs>
+  </div>
+</template>
+<script>
+import Tabs from '@/components/Tabs.vue'
+import Tab from '@/components/Tab.vue'
+  export default {
+    components: {
+      Tabs,
+      Tab,
+    }
+  }
+</script>
+
