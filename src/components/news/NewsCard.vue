2df6f860899fef354ae37e97953012ea1164db35
うに <114372516+yu-xian-chen@users.noreply.github.com>
2022-11-30 19:16:18 +0800

    add Newscard component、消息內容引進

---
+++ src/components/news/NewsCard.vue
@@ -0,0 +1,97 @@
+<template>
+    <div class="news-card-img">
+        <!-- <img :src="link" alt=""> -->
+        <!-- <img src="../assets/img/News/1.jpg" alt=""> -->
+        
+    </div>
+    <div class="news-card-txt">
+        <p class="news-date">
+            <span class="type">{{type}}</span>
+            {{date}}
+        </p>
+        <h3 class="news-title">{{title}}</h3>
+        <p class="news-des">{{des}}</p>
+        <div class="news-btn">
+            <a href="#" class="news-btn-more">more</a>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: '[NewsCard]',
+    props: {
+        type: String,
+        date: String,
+        title: String,
+        des: String,
+        link:String
+    }
+}
+</script>
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+/* 圖片 */
+.news-card-img{
+    width: 50%;
+    margin: auto;
+}
+.news-card-img img{
+    width: 100%;
+}
+
+.news-date{
+    font-size: 14px;
+    margin-bottom: 20px;
+}
+.type{
+    background-color: #BC955C;
+    color: #fff;
+    padding: 5px 5px;
+    margin-right: 10px;
+}
+
+.news-title{
+    font-size: 20px;
+    margin-bottom: 20px;
+
+}
+.news-des{
+    font-size: 16px;
+    color: #888888;
+    margin-bottom: 60px;
+
+}
+  /* 按鈕 */
+.news-btn{
+    width: fit-content;
+    margin: auto;
+}
+.news-btn-more{
+    font-size: 20px;
+    color: #888888;
+    display: inline-block;
+    padding-bottom: 5px;
+    position: relative;
+
+}
+
+.news-btn-more:hover{
+    content: '';
+    border-bottom: 1px solid #BC955C;
+}
+
+@media screen and (max-width:767px){
+
+}
+@media screen and (min-width: 768px){
+
+}
+
+@media screen and (min-width: 1024px){
+
+}
+</style>
--- src/views/NewsView.vue
+++ src/views/NewsView.vue
@@ -2,33 +2,59 @@
   <Header/>
   <Banner/>
   <breadcrumb/>
-  <p>Hello</p>
+  <div class="container">
+    <div class="news-cards">
+      <!-- 組件 tab -->
+        <div class="news-card">
+            <!-- 組件 -->
+            <!-- 1 -->
+            <NewsCard 
+                link="require(`@/assets/img/1.jpg"
+                type="活動" 
+                date="2022/10/02" 
+                title="高千穗-夜神樂" 
+                des="高千穗知名祭典活動-夜神樂 ，將於今年的11月..." 
+            />
+            
+
+            
+        </div>
+  </div>
+  <div class="news-next-page">
+      <!-- 組件 -->
+  </div>
+    <div class="go-up">
+        <!-- 組件 -->
+    </div>
+  </div>
 
 
 
 
-<pre style="text-align:start">
-  footer尚未完成:
-  ▪ hover效果?
-  ▪ 文字顏色ffffff待調整(?
-  ▪ 換行文字不居中
-  ▪ 手機板背景圖調整
-</pre>
-  <Footer/>
 </template>
+
+<style scoped lang="scss">
+.container{
+  max-width: 1200px;
+  margin: auto;
+}
+</style>
+
 <script>
   import Header from "@/components/Header.vue"
   import Banner from "@/components/Banner.vue"
   import breadcrumb from "@/components/breadcrumb.vue"
+  import NewsCard from "@/components/news/NewsCard.vue"
   import Footer from "@/components/Footer.vue"
 
   export default {
       name: "NewsView",
       components:{
           Header,
+          Banner,
           breadcrumb,
-          Footer,
-          Banner
+          NewsCard,
+          Footer
       }
   }
 </script>
