b7289c71a091e0d2365fd54695d210d0ce709c3c
tangtang1b1b <a0981632653@gmail.com>
2022-12-01 16:29:39 +0800

    可芸推推

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -11,7 +11,7 @@
                     </li>
                 </ul>
             <div class="h_feature">
-                <a href=""><i class="fa-regular fa-user"></i></a>
+                <router-link class="go" to ="/login"><i class="fa-regular fa-user"></i></router-link>
                 <a href=""><i class="fa-solid fa-cart-shopping"></i></a>
                 <div id="hambur" class="hambur" @click="toggle = !toggle">
                     <div class="hambox"></div>
