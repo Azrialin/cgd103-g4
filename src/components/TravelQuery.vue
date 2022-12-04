60d80570b4f42e49612aeb9ba570b292e0d6364f
Azrialin <zxe2124@gmail.com>
2022-12-04 22:29:48 +0800

    travelquery rwd done

--- src/components/TravelQuery.vue
+++ src/components/TravelQuery.vue
@@ -36,8 +36,168 @@
                     </div>
               </div>
               <a href="" class="bottom">查看詳情</a>
-              <button class="btn-gold_2nd">返回</button>
             </div>
+
+            <div class="travel-wrapper">
+              <div class="travel-detail">
+                    <div class="travel-info">
+                        <h3>訂單編號 </h3>
+                        <span>{{result.orderNo}}</span>
+                    </div>
+                 <hr>
+                    <div class="travel-info">
+                        <h3>行程方案 </h3>
+                        <span>{{result.package}}</span>
+                    </div>
+                 <hr>
+                    <div class="travel-info">
+                        <h3>行程日期 </h3>
+                        <span>{{result.departureDate}}</span>
+                    </div>
+                 <hr>
+                    <div class="travel-info">
+                        <h3>報名人數 </h3>
+                        <span>{{result.noOfTraveler}}</span>
+                    </div>
+                 <hr>
+                    <div class="travel-info">
+                        <h3>總計金額 </h3>
+                        <span>{{result.total}}</span>
+                    </div>
+                 <hr>
+                    <div class="travel-info">
+                        <h3>訂單狀態 </h3>
+                        <span>{{result.paymentStatus}}</span>
+                    </div>
+              </div>
+              <a href="" class="bottom">查看詳情</a>
+            </div>
+
+            <div class="travel-wrapper">
+              <div class="travel-detail">
+                    <div class="travel-info">
+                        <h3>訂單編號 </h3>
+                        <span>{{result.orderNo}}</span>
+                    </div>
+                 <hr>
+                    <div class="travel-info">
+                        <h3>行程方案 </h3>
+                        <span>{{result.package}}</span>
+                    </div>
+                 <hr>
+                    <div class="travel-info">
+                        <h3>行程日期 </h3>
+                        <span>{{result.departureDate}}</span>
+                    </div>
+                 <hr>
+                    <div class="travel-info">
+                        <h3>報名人數 </h3>
+                        <span>{{result.noOfTraveler}}</span>
+                    </div>
+                 <hr>
+                    <div class="travel-info">
+                        <h3>總計金額 </h3>
+                        <span>{{result.total}}</span>
+                    </div>
+                 <hr>
+                    <div class="travel-info">
+                        <h3>訂單狀態 </h3>
+                        <span>{{result.paymentStatus}}</span>
+                    </div>
+              </div>
+              <a href="" class="bottom">查看詳情</a>
+            </div>
+
+            <div class="travel-wrapper">
+              <div class="travel-detail">
+                    <div class="travel-info">
+                        <h3>訂單編號 </h3>
+                        <span>{{result.orderNo}}</span>
+                    </div>
+                 <hr>
+                    <div class="travel-info">
+                        <h3>行程方案 </h3>
+                        <span>{{result.package}}</span>
+                    </div>
+                 <hr>
+                    <div class="travel-info">
+                        <h3>行程日期 </h3>
+                        <span>{{result.departureDate}}</span>
+                    </div>
+                 <hr>
+                    <div class="travel-info">
+                        <h3>報名人數 </h3>
+                        <span>{{result.noOfTraveler}}</span>
+                    </div>
+                 <hr>
+                    <div class="travel-info">
+                        <h3>總計金額 </h3>
+                        <span>{{result.total}}</span>
+                    </div>
+                 <hr>
+                    <div class="travel-info">
+                        <h3>訂單狀態 </h3>
+                        <span>{{result.paymentStatus}}</span>
+                    </div>
+              </div>
+              <a href="" class="bottom">查看詳情</a>
+            </div>
+
+            <div class="travel-wrapper-xl">
+              <!-- <div class="travel-detail"> -->
+                <div class="travel-info-xl">
+                    <h3>訂單編號</h3>
+                    <h3>行程方案</h3>
+                    <h3>行程日期</h3>
+                    <h3>報名人數</h3>
+                    <h3>總計金額</h3>
+                    <h3>訂單狀態</h3>
+                    <h3>訂單詳情</h3>
+                </div>
+                <div class="travel-info-status">
+                    <h3>{{result.orderNo}}</h3>
+                    <h3>{{result.package}}</h3>
+                    <h3>{{result.departureDate}}</h3>
+                    <h3>{{result.noOfTraveler}}</h3>
+                    <h3>{{result.total}}</h3>
+                    <h3>{{result.paymentStatus}}</h3>
+                    <a href="">查看詳情</a>
+                </div>
+                <div class="travel-info-status">
+                    <h3>{{result.orderNo}}</h3>
+                    <h3>{{result.package}}</h3>
+                    <h3>{{result.departureDate}}</h3>
+                    <h3>{{result.noOfTraveler}}</h3>
+                    <h3>{{result.total}}</h3>
+                    <h3>{{result.paymentStatus}}</h3>
+                    <a href="">查看詳情</a>
+                </div>
+                <div class="travel-info-status">
+                    <h3>{{result.orderNo}}</h3>
+                    <h3>{{result.package}}</h3>
+                    <h3>{{result.departureDate}}</h3>
+                    <h3>{{result.noOfTraveler}}</h3>
+                    <h3>{{result.total}}</h3>
+                    <h3>{{result.paymentStatus}}</h3>
+                    <a href="">查看詳情</a>
+                </div>
+                <div class="travel-info-status">
+                    <h3>{{result.orderNo}}</h3>
+                    <h3>{{result.package}}</h3>
+                    <h3>{{result.departureDate}}</h3>
+                    <h3>{{result.noOfTraveler}}</h3>
+                    <h3>{{result.total}}</h3>
+                    <h3>{{result.paymentStatus}}</h3>
+                    <a href="">查看詳情</a>
+                </div>
+              <!-- </div> -->
+            </div>
+            
+
+            <!-- 返回button -->
+            <button class="btn-gold_2nd">返回</button>
+
+            
 
 
         </div>
@@ -110,8 +270,8 @@ export default {
     border-radius: 10px 10px 0 0;
   }
   .travel-info{
-      display: flex;
-      justify-content: flex-start;
+    display: flex;
+    justify-content: flex-start;
   }
   .travel-info h3,.travel-info span{
     display: inline-block;
@@ -122,68 +282,58 @@ export default {
     background: #BC955C;
     border: none;
   }
+
   .bottom{
-    margin: 20px 0;
+    margin: 20px 0 0;
     display: flex;
     justify-content: flex-end;
     color: #BC955C;
     text-decoration-line: underline;
     // border-bottom:1px solid #BC955C;
   }
-//   .must{
-//     color: #941111;
-//     display: flex;
-//     justify-content: flex-end;
-//     // margin-right: 20px;
-//   }
-//   .info{
-//     // margin-left: 10%;
-//     display: flex;
-//     flex-wrap: wrap;
-//     justify-content: flex-start;
-//   }
-  
-//   input,select,option{
-//     width: 100%;//input 右邊沒有吃到form class 的10% padding要再問學長
-//     margin: 20px 0;
-//     margin-right: 10%;
-//     padding: 10px;
-//     border-radius: 10px;
-//     border: 1px solid #BC955C ;
-//     outline:none;
-//     text-align: left;
-//     box-sizing: border-box;
-//     // display: flex;
-//     // flex-wrap: wrap;
-//   }
-//   select{
-//     width: 100%;
-//     appearance: none;
-//     background: url("../assets/img/🦆 icon _chevron-down_.svg")90% 50% no-repeat scroll transparent;
-//   }
-//   .unchangeable{
-//      border: 1px solid #CCC ;
-//      background: #e6e6e6;
-//      color: #888;
-//   }
-//   span{
-//     // color: #941111;
-//   }
-//   .button-section{
-//     margin-top: 5%;
-//     display:flex;
-//     flex-direction: row-reverse;
-//   }
-//   .button-section .btn-gold_2nd{
-//     margin-left: 20px;
-//   }
+  .btn-gold_2nd{
+    display:flex;
+    justify-content: flex-start;
+    margin: 0 10%;
+  }
+
+  .travel-wrapper-xl{
+    padding: 10%;
+    padding-top: 20px;
+    display: none;
+  }
+  .travel-info-xl{
+        border-radius: 10px 10px 0 0;
+        background: #BC955C;
+        display: flex;
+        justify-content: space-evenly;
+        color: white;
+        padding: 20px;
+  }
+
+  .travel-info-status{
+    display: flex;
+    justify-content: space-evenly;
+    border: 1px solid #BC955C;
+    padding: 20px;
+    text-align: center; //沒辦法對齊
+  }
+  .travel-info-status a{
+    color: #BC955C;
+    text-decoration-line: underline;
+  }
+
 
 @media (min-width:1200px) {
   
-  .try{
-    display: inline-block;
-    justify-content: start;
-    // box-sizing: border-box;
+  .travel-wrapper{
+    display: none;
+  }
+  .travel-wrapper-xl{
+    display: block;
+  }
+  .btn-gold_2nd{
+    margin-top: -50px;
   }
 }
 
