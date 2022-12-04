740c9ae08711fcef7e115da28dd365956dd73f7b
Yun097 <cream86099797@gmail.com>
2022-12-04 19:44:58 +0800

    add forgotpassword

--- src/views/SignupView.vue
+++ src/views/SignupView.vue
@@ -15,22 +15,24 @@
                     <Input/>
                     <h3>電話號碼<span>*</span></h3>
                     <Input/>
-                    <input class="agreebox" type="checkbox" value="ok"/>
-                    <label for="agree">同意我們的<a href="">使用條款</a>和<a href="">隱私政策</a></label>
+                    <div class="box">
+                      <input type="checkbox" id="agreebox" name="agreebox" value="ok"/>
+                      <label for="agreebox">同意我們的<a href="">使用條款</a>和<a href="">隱私政策</a></label>
+                    </div>
+                    <button class="btn-gold">註冊為會員</button>
+                    <a class="back" href="">回上一頁</a>
                 </form>
-                <button class="btn-gold">註冊為會員</button>
-                <a href="">回上一頁</a>
             </div>
         </div>
     </div>
-</template>
-
-<script>
+  </template>
+  
+  <script>
     import breadcrumb from "@/components/breadcrumb.vue"
     import Input from "@/components/Input.vue"
-
+  
     export default {
-        name: 'loginView',
+        name: 'SignupView',
         components: {
             breadcrumb,
             Input
@@ -42,8 +44,105 @@
             };
         },
     };
-</script>
-<style lang="scss" scoped>
-@import "../assets/scss/base/font.scss";
-@import "../assets/scss/base/color.scss";
-@import "../assets/scss/components/btn.scss";
+  </script>
+  <style lang="scss">
+  @import "../assets/scss/base/font.scss";
+  @import "../assets/scss/base/color.scss";
+  @import "../assets/scss/components/btn.scss";
+  
+  .signup{
+      .main{
+          @include minW(1200px){
+                  width: calc(60% - 100px);
+          }
+          height: 1000px;
+          margin: 10%;
+          margin: auto;
+          // border-radius: 10px;
+          // border: 1px solid $front_color_main;
+          // box-shadow: 0 8px 10px -5px rgb(198, 198, 198);
+  
+          form{
+              width: 320px;
+              margin: auto;
+              margin-top: 50px;
+              text-align: right;
+  
+              h2{
+                  @include font(32px);
+                  color: $color_444;
+                  padding: 5px 0;
+              }
+  
+              hr{
+                  width: 100%;
+                  height: 0.5px;
+                  margin-bottom: 50px;
+                  background: #BC955C;
+                  border: none;
+              }
+  
+              h3{
+                  @include font(18px);
+                  color: $color_444;
+                  font-weight: 300;
+                  padding: 20px 0;
+              }
+  
+              p{
+                @include font(15px);
+                color: #941111;
+                text-align: right;
+              }
+  
+              span{
+                color: #941111;
+              }
+  
+              Input{
+                  width: 300px !important;
+                  margin-bottom: 20px;
+              }
+  
+              .box{
+                  display: flex
+              }
+  
+              #agreebox{
+                  width: 20px !important;
+              }
+  
+              label{
+                  @include font(15px);
+                  color: $color_444;
+                  padding: 0 5px;
+                  a{
+                    color: $front_color_main;
+                    &:hover{
+                      color: $front_color_active;
+                    }
+                  }
+              }
+  
+              button{
+                width: 170px !important;
+                padding: 10px 10px !important;
+                margin-bottom: 100px;
+                margin-top: 80px;
+                margin-right: 80px;
+              }
+              .back{
+                @include font(15px);
+                  font-weight: 300;
+                  color:$color_444;
+                  text-decoration:none;
+            
+                  &:hover {
+                  text-decoration:none;
+                  color:$front_color_main;
+                  }
+              }
+          }   
+      }
+  }
+  </style>
