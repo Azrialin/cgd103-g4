d536ab94c9f9b571fc5868cd62c4613bcd6b8e3f
chubbyrabi <chubbytobey@gmail.com>
2022-12-27 16:27:27 +0800

    改好了 可以測試了

--- src/views/RailwayTour.vue
+++ src/views/RailwayTour.vue
@@ -95,8 +95,8 @@
             forgotPsw(){
                 console.log(this.mem_email);
                 // fetch(`${BASE_URL}/forgotPsw.php`)
-                fetch("http://localhost/CGD103_G4_front/public/phpfiles/forgotPsw.php",{
-                // fetch("http://localhost/CGD103_G4_front/public/phpfiles/PHPMailer-6.7.1/src/forgotPsw.php",{
+                // fetch("http://localhost/CGD103_G4_front/public/phpfiles/forgotPsw.php",{
+                fetch(`${BASE_URL}/forgotPsw.php`,{
                     method:'POST', body:new URLSearchParams({
                         mem_email:this.mem_email,
                 })})
