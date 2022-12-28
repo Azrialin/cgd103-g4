2982ea64a35ad67f26b4b53c0139f7f9e27c81db
tangtang1b1b <a0981632653@gmail.com>
2022-12-29 01:58:56 +0800

    訂ㄍ高度

--- src/views/TravelView.vue
+++ src/views/TravelView.vue
@@ -136,6 +136,8 @@
             }
             img{
               width: 100%;
+              height: 400px;
+              object-fit: cover;
               border-radius: 10px;
             }
           }
