4547c798606af5e3e33aa2b76fc92f57a413a223
chubbyrabi <chubbytobey@gmail.com>
2023-01-02 01:45:24 +0800

    add FAQ 搜尋功能 ( 裝了一個mark.js 套件，需要 npm i )

--- public/phpfiles/Faq_getData.php
+++ public/phpfiles/Faq_getData.php
@@ -4,12 +4,17 @@
     
 	require_once("./connect_cgd103g4.php");
     // sql 指令
-    $sql = "select * from `faq`";
+    $sql = "select * from `faq` where faq_status = 1";
     $faq = $pdo->query($sql);
     $faqs = $faq->fetchAll();
-    $data=[];
     foreach($faqs as $i=> $page){
-          $data[]=$page;
+        $data[]=[
+            'faq_no' => strval($page['faq_no']),
+            'faq_type' => $page['faq_type'],
+            'faq_q' => $page['faq_q'],
+            'faq_a' => $page['faq_a'],
+            'faq_status' => intval($page['faq_status'])
+        ];
     }
     echo json_encode($data);
 ?>
